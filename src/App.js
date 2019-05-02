7d751e21eb47a49b939884ade3c0f2c71939427c
thechocobowhisperer <the.chocobo.whisperer@gmail.com>
2019-05-01 20:25:37 -0500

    Write phrase that changes cat to dog on button click

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, {Component} from 'react';
 
+class App extends Component{
+  state ={animal:'cat'}
+  changeAnimal = (furbaby)  => {
+    console.log(furbaby.target.name)
+    let newAnimal = this.state.animal
+    newAnimal = 'dog'
+    
+    console.log(furbaby.target.value)
+
+    this.setState({animal: newAnimal})
+  } 
+  
+  render() {
+      return(
+        <div>
+          <h2>You are a {this.state.animal} lover. How wonderful!</h2>
+          <Button color="primary" onClick={(dog) => this.changeAnimal(dog)}>
+            Submit favorite animal
+          </Button> 
+        </div>
+      );
+  };
+};
+ 
 export default App;
